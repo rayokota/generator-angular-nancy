9e527c7e414a0a9fea73c677ef4b1eee40df8fa8
Robert Yokota <ryokota@yammer-inc.com>
2014-03-20 13:35:54 -0700

    Support running in bg with nohup on Mono

--- app/templates/_App/_Main.cs
+++ app/templates/_App/_Main.cs
@@ -1,6 +1,7 @@
 using System;
 using Nancy.Hosting.Self;
 using System.Diagnostics;
+using System.Threading;
 
 namespace <%= _.capitalize(baseName) %>
 {
@@ -19,11 +20,16 @@ namespace <%= _.capitalize(baseName) %>
             nancyHost.Start();
             
             Console.WriteLine("Nancy now listening at http://localhost:8080/. Press enter to stop");
-            //Process.Start("http://localhost:8080/");
-            Console.ReadKey();
+            ConsoleKeyInfo key = Console.ReadKey();
+            if ((int)key.Key == 0) 
+            {
+                // Mono returns a ConsoleKeyInfo with a Key value of 0 when stdin is redirected
+                // See https://bugzilla.xamarin.com/show_bug.cgi?id=12551
+                // For now, just sleep, so that we can run in background with nohup
+                Thread.Sleep(Timeout.Infinite);
+            }
             
             nancyHost.Stop();
-            
             Console.WriteLine("Stopped. Good bye!");
         }
     }
